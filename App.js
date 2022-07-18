1ab782702a37e5ad847bb86ce8211ce1ce97da68
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-18 16:33:16 -0700

    fjfhfgh

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff' ,
     alignItems: 'center',
     justifyContent: 'center',
   },

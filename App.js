a88c2af7240f3fb23fb635a7868b1f55b94e21a1
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-18 19:15:49 -0700

    ddhd

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import orders from './assets/data/orders.json';
 import { Entypo } from '@expo/vector-icons';
 import OrderItem from './src/components/OrderItem';
+import OrdersScreen from './src/screens/OrdersScreen';
+
 const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <OrderItem order={order}/>
-      <OrderItem order={orders[1]}/>
-      <OrderItem order={orders[2]}/>
+      {/* <FlatList 
+        data={orders}
+        renderItem={({item})=> <OrderItem order={item}/>}
+        /> */}
+        <OrdersScreen/>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,5 +25,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff' ,
     justifyContent: 'center',
+    paddingTop: 50,
   },
 });
---
+++ src/screens/OrdersScreen/index.js
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+import {View, Text} from 'react-native'
+
+const OrdersScreen = () => {
+    return(
+        <View>
+            <Text>fhehegii</Text>
+        </View>
+    )
+}
+
+export default OrdersScreen;

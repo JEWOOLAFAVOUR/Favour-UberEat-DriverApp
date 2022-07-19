159b49baaea50f6aa1859499d877a9a5f24fc75e
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-18 19:39:49 -0700

    jfjfjj

--- src/screens/OrdersScreen/index.js
+++ src/screens/OrdersScreen/index.js
@@ -1,10 +1,17 @@
 import { useRef } from "react";
 import {View, Text} from 'react-native'
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const OrdersScreen = () => {
+    const bottomSheetReef = useRef(null); 
     return(
-        <View>
-            <Text>fhehegii</Text>
+        <View style={{backgroundColor: 'blue', flex: 1}}>
+            <BottomSheet ref={bottomSheetReef} index={1}
+            snapPoints={['12%', '95%']}>
+                <View style={{flex: 1}}>
+                    <Text>Hello</Text>
+                </View>
+            </BottomSheet>
         </View>
     )
 }

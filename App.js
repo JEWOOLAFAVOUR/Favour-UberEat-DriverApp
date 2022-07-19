9a125bff2a86778e5d4ea6cacaa9a1868b759359
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-18 17:50:12 -0700

    jdheh

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import orders from './assets/data/orders.json'
+import orders from './assets/data/orders.json';
+import { Entypo } from '@expo/vector-icons';
 
 const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{flexDirection: 'row', 
+      borderColor: '#3fc060', margin: 10,
+      borderWidth: 2, borderRadius: 10,
+      justifyContent: 'space-between'}}>
         <Image source={{
           uri: order.Restaurant.image
-        }} style={{width: 50, height: 50}}/>
-      <Text>{order.Restaurant.name}</Text>
-      <Text>{order.Restaurant.address}</Text>
-      <Text>{order.User.name}</Text>
-      <Text>{order.User.address}</Text>
+        }} style={{width: '25%', height: '100%'}}/>
+      <View style={{marginLeft:10}}>
+        <Text>{order.Restaurant.name}</Text>
+        <Text>{order.Restaurant.address}</Text>
+        <Text>{order.User.name}</Text>
+        <Text>{order.User.address}</Text>
+      </View>
+      <Entypo name='check' size={30} color='black'
+        style={{marginLeft: 'auto'}}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,7 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff' ,
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
